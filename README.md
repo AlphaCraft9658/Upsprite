96fef5910ea007f2789d43001f9d845c51f6d6f9
Alpha-Craft <knolld00@gmail.com>
2023-12-20 09:21:35 +0100

    Initial setup Contains the build script and readme. Build script will be tested more in-depth

---
+++ README.md
@@ -0,0 +1,29 @@
+## Installation
+Before running the install script, make sure your distro specific dependencies listed in the Dependencies section are installed or just run the correct install command.
+For installing Aseprite make sure upsprite.sh has execution permissions, simply run ./upsprite and follow any promtps given.
+If you want to automate the installation with your own scripts and integrate upsprite, or just quickly run without the prompts, you can run the script with `--prebuilt` to automatically use the prebuilt skia, `--rebuild_skia` for automatically removing and rebuilding/redownloading skia and `--full` for automatically building skia, independant of whether there is a prebuilt package available or not.
+
+## Dependencies
+
+You will need the following dependencies on Ubuntu/Debian:
+
+    sudo apt-get install -y g++ clang libc++-dev libc++abi-dev cmake ninja-build libx11-dev libxcursor-dev libxi-dev libgl1-mesa-dev libfontconfig1-dev
+
+Or use clang-10 packages (or newer) in case that clang in your distribution is older than clang 10.0:
+
+    sudo apt-get install -y clang-10 libc++-10-dev libc++abi-10-dev
+
+On Fedora:
+
+    sudo dnf install -y gcc-c++ clang libcxx-devel cmake ninja-build libX11-devel libXcursor-devel libXi-devel mesa-libGL-devel fontconfig-devel
+
+On Arch:
+
+    sudo pacman -S gcc clang libc++ cmake ninja libx11 libxcursor mesa-libgl fontconfig
+
+On SUSE:
+
+    sudo zypper install gcc-c++ clang libc++-devel libc++abi-devel cmake ninja libX11-devel libXcursor-devel libXi-devel Mesa-libGL-devel fontconfig-devel
+
+## If you encounter issues
+If you encounter any issues, either try to find the cause from the command line output or submit an issue on the upsprite github repo.
---
+++ upsprite.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+full=false
+prebuilt=false
+rebuild=false
+
+while [ $# -gt 0 ]; do
+  if [[ $1 == "--full" && "$prebuilt" = false ]]; then
+    full=true
+  elif [[ $1 == "--prebuilt" && "$full" = false ]]; then
+    prebuilt=true
+  elif [ $1 == "--rebuild-skia" ]; then
+    rebuild=true
+  fi
+  shift
+done
+
+echo "----- Fetching Aseprite source -----"
+if [ ! -d "$SCRIPT_DIR/aseprite" ]; then
+  git clone --recursive https://github.com/aseprite/aseprite.git
+else
+  cd aseprite
+  git pull
+  git submodule update --init --recursive
+  if [ -d "build" ]; then
+    rm -rf build
+  fi
+  cd ..
+fi
+
+build_skia() {
+  echo "----- Building skia -----"
+  mkdir deps && cd deps
+  git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+  git clone -b aseprite-m102 https://github.com/aseprite/skia.git
+  export PATH="${PWD}/depot_tools:${PATH}"
+  cd skia
+  python tools/git-sync-deps
+  gn gen out/Release-x64 --args='is_debug=false is_official_build=true skia_use_system_expat=false skia_use_system_icu=false skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_zlib=false skia_use_sfntly=false skia_use_freetype=true skia_use_harfbuzz=true skia_pdf_subset_harfbuzz=true skia_use_system_freetype2=false skia_use_system_harfbuzz=false cc="clang" cxx="clang++" extra_cflags_cc=["-stdlib=libc++"] extra_ldflags=["-stdlib=libc++"]'
+  ninja -C out/Release-x64 skia modules
+}
+
+skia_setup() {
+  if $full; then
+    echo "Building skia"
+    build_skia
+    return
+  fi
+  if [[ $(uname -m) == "x86_64" && "$prebuilt" = true ]]; then
+    curl -LO https://github.com/aseprite/skia/releases/download/m102-861e4743af/Skia-Linux-Release-x64-libc++.zip
+    mkdir deps
+    unzip Skia-Linux-Release-x64-libc++.zip -d deps/skia
+    rm Skia-Linux-Release-x64-libc++.zip
+  elif [[ $(uname -m) == "x86_64" && $prebuilt = false ]]; then
+    input="x"
+    printf "There is a prebuilt package of skia for your current architecture.\nDo you want to use the prebuilt archive? This will speed up the build process! (NOTE: If you encounter any issues while using the prebuilt package, try to re-run this script and tick n instead) (y/n): "
+    set +e
+    while [[ $input != "y" && $input != "n" ]]; do
+      read -rsn1 input
+      input=$(echo $input | tr '[:upper:]' '[:lower:]')
+      if [ "$input" == "y" ]; then
+        set -e
+        echo
+        curl -LO https://github.com/aseprite/skia/releases/download/m102-861e4743af/Skia-Linux-Release-x64-libc++.zip
+        mkdir deps
+        unzip Skia-Linux-Release-x64-libc++.zip -d deps/skia
+        rm Skia-Linux-Release-x64-libc++.zip
+      elif [ "$input" == "n" ]; then
+        set -e
+        printf "\n"
+        build_skia
+      fi
+    done
+  elif [ $prebuilt = false]; then
+    printf "There is no prebuilt package of skia for your architecture. Automatically building skia.\n"
+    build_skia
+  elif [ $prebuilt = true]; then
+    printf "The is no prebuilt package of skia for your architecture. Building skia anyway.\n"
+    build_skia
+  fi
+}
+
+echo "----- Skia setup -----"
+if [[ -d "$SCRIPT_DIR/deps" && "$rebuild" = false ]]; then
+  printf "The directory $SCRIPT_DIR/deps already exists.\nIf you have encountered issues issues with the prebuilt archive in a previous attempt or want to rebuild/re-download skia, you should remove the directory first.\nDo you want to do that? (y/n): "
+  input="x"
+  set +e
+  while [[ $input != "y" && $input != "n" ]]; do
+    read -rsn1 input
+    input=$(echo $input | tr '[:upper:]' '[:lower:]')
+    if [ "$input" == "y" ]; then
+      echo
+      set -e
+      rm -rf deps
+      skia_setup
+    fi
+  done
+elif [ "$rebuild" = true ]; then
+  rm -rf deps
+  skia_setup
+else
+  skia_setup
+fi
+
+echo "----- Building Aseprite -----"
+cd $SCRIPT_DIR/aseprite && mkdir build && cd build
+export CC=clang
+export CXX=clang++
+cmake \
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  -DCMAKE_CXX_FLAGS:STRING=-stdlib=libc++ \
+  -DCMAKE_EXE_LINKER_FLAGS:STRING=-stdlib=libc++ \
+  -DLAF_BACKEND=skia \
+  -DSKIA_DIR=$SCRIPT_DIR/deps/skia \
+  -DSKIA_LIBRARY_DIR=$SCRIPT_DIR/deps/skia/out/Release-x64 \
+  -DSKIA_LIBRARY=$SCRIPT_DIR/deps/skia/out/Release-x64/libskia.a \
+  -G Ninja \
+  ..
+ninja aseprite
+
+ln -sf $SCRIPT_DIR/aseprite/build/bin/aseprite $HOME/.local/bin/aseprite
+echo "[Desktop Entry]
+Exec=aseprite
+Icon=$SCRIPT_DIR/aseprite/build/bin/data/icons/ase256.png
+Name=Aseprite
+NoDisplay=false
+StartupNotify=true
+Terminal=false
+Type=Application
+X-KDE-SubstituteUID=false" > $HOME/.local/share/applications/Aseprite.desktop
